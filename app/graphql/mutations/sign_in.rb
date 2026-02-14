33b4b3cf0b9c67664da7e94c1fccd5ee552bf6e3
nobu09 <11857481+nobu09@users.noreply.github.com>
2026-02-14 19:35:22 +0900

    SignIn Mutationのリクエストパラメータを設定（email, password）

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -1,2 +1,8 @@
 class Mutations::SignIn < Mutations::BaseMutation
+  null true
+  argument :email, String, required: true
+  argument :password, String, required: true
+
+  def resolve(email:, password:)
+  end
 end
